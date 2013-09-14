ed3f9550bfbe43e4a53cb46fe7714512003a3b9c
Ken Liu <ken.liu@gmail.com>
2013-09-14 11:13:47 -0400

    update to 1.5.6

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -12,10 +12,10 @@ grails.project.dependency.resolution = {
     }
 
     dependencies {
-        build ('com.amazonaws:aws-java-sdk:1.5.5') { //this depends on HttpClient 4.1, which seems to clash with HttpClient 4.0 from Grails
+        build ('com.amazonaws:aws-java-sdk:1.5.6') { //this depends on HttpClient 4.1, which seems to clash with HttpClient 4.0 from Grails
             //export = false
         }
-        compile ('com.amazonaws:aws-java-sdk:1.5.5') { //this depends on HttpClient 4.1, which seems to clash with HttpClient 4.0 from Grails
+        compile ('com.amazonaws:aws-java-sdk:1.5.6') { //this depends on HttpClient 4.1, which seems to clash with HttpClient 4.0 from Grails
             //export = false
         }
         runtime 'org.yaml:snakeyaml:1.13'
